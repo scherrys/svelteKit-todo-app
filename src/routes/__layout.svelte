bcae16482f0d4d1ad30ba80c3f1b5ee0f8a98e76
Jenny Scherr <jenny.scherr@svt.se>
2022-05-01 17:55:23 +0200

    login WIP

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,22 @@
 <script>
+    import { supabase } from '../supabase.js'
+    import Auth from '../components/Auth.svelte'
     import '../app.css'
+    import {user} from '../stores/authStore'
+    import { loadTodos } from '../stores/todoStore.js';
+
+    console.log("auth : ", supabase.auth.user())
+    user.set(supabase.auth.user())
+
+    supabase.auth.onAuthStateChange((_,session) => {
+        user.set(session?.user)
+        if(session?.user){
+            loadTodos
+        }
+    })
 </script>
   
 <div class="container mx-auto my-6 max-w-lg">
+    <Auth />
     <slot />
 </div>
--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -1,22 +1,46 @@
 import { writable } from "svelte/store";
+import { supabase } from "../supabase";
 
 export const todos = writable([])
 
-export const addTodo = (text) => {
-    todos.update(cur => {
-        const newTodos = [ ...cur, { text, completed: false, id: Date.now() }]
-        return newTodos
-    })
+export const loadTodos = async () => {
+
+    const {data, error} = await supabase.from('todos').select()
+
+    if (error) {
+        return console.error(error)
+    }
+    todos.set(data)
 }
 
-export const deleteTodo = (id) => {
+export const addTodo = async(text) => {
+    const { data, error } = await supabase.from('todos').insert([{text, completed: false}])
+
+    if (error) {
+        return console.error(error)
+    }
+
+    todos.update(cur => [ ...cur, data[0]])
+}
+
+export const deleteTodo = async(id) => {
+    const { error } = await supabase.from('todos').delete().match({id})
+    if (error) {
+        return console.error(error)
+    }
+
     todos.update( todos => todos.filter(todo => todo.id !== id))
 }
 
-export const toggleTodoCompleted = (id) => {
+export const toggleTodoCompleted = async(id, currentState) => {
+    const { error } = await supabase.from('todos').update({ completed: !currentState }).match({id})
+    if(error) {
+        return console.error(error)
+    }
+
     todos.update(todos => {
         let index = -1
-        // for(i = 0, i < todos.length, i++){
+
         for (let i = 0; i < todos.length; i++) {
             if(todos[i].id === id) {
                 index = i
